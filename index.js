3f23798e2d240bfa3a5cc612abc613f12638d819
simee56 <pandeysimee7@gmail.com>
2025-02-20 22:38:00 +0530

    import mongoose, signup function and use middleware

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const path = require('path');
+const connectMongoDB = require('./connection');
 
 const app = express();
 const PORT = 3000;
 
-//MIDDLEWARES
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'))
 
-app.get('/', (req,res)=> {
-    res.render("home")
+
+//connection
+connectMongoDB("mongodb://127.0.0.1:27017/Blog").then(() =>
+    console.log("MongoDb connected")
+);
+
+
+//Routes
+const staticRoute = require('./routes/staticRouter');
+const userRoute = require('./routes/user');
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));    //this is for form data
+
+
+app.use('/', staticRoute);                 // static router = frontend pages
+app.use('/user', userRoute);
+
+app.get('/', (req, res) => {
+    res.render("home");
 })
+
+
 app.listen(PORT, () => {
     console.log(`SERVER HAS STARTED AT THE PORT ${PORT}`);
 })
