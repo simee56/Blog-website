b3eccc46dc438ef79d015352027c4fb6bca7c747
simee56 <pandeysimee7@gmail.com>
2025-02-20 23:04:48 +0530

    post route for signin

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {handleUserSignUp} = require('../controllers/user')
+const {handleUserSignUp, handleUserSignIn} = require('../controllers/user')
 
 router.post('/signup', handleUserSignUp);
+router.post('/signin', handleUserSignIn);
+
 
 module.exports = router;
