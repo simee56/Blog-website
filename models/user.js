f609d2ec80affa6925880f10df714a046972b35e
simee56 <pandeysimee7@gmail.com>
2025-02-20 20:51:51 +0530

    create user schema amd user model

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+const { createHmac, randomBytes } = require('crypto');
+
+const userSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: {      //Salt is a random string.
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePicURL: {
+        type: String,
+        default: "./images/image.png"
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    },
+},
+    { timestamps: true }
+);
+
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified("password"))
+        return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt)
+        .update(user.password)
+        .digest("hex");
+
+    this.salt = salt;
+    this.passowrd = hashedPassword;
+
+    next();
+})
+
+
+
+const User = model("user", userSchema);
+
+module.exports = User;
