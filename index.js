e48dc8a2e447f78bfc687885375c8f16f2dbcd4b
simee56 <pandeysimee7@gmail.com>
2025-02-22 13:25:03 +0530

    use cookieParser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const connectMongoDB = require('./connection');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = 3000;
@@ -19,19 +20,23 @@ connectMongoDB("mongodb://127.0.0.1:27017/Blog").then(() =>
 //ROUTES
 const staticRoute = require('./routes/staticRouter');
 const userRoute = require('./routes/user');
+const checkForAuthenticationCookie = require('./middleware/authentication');
+
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: false }));    //this is for form data
-
+app.use(checkForAuthenticationCookie("token"))
+app.use(cookieParser());
 
 app.use('/', staticRoute);                 // static router = frontend pages
 app.use('/user', userRoute);
 
 
 app.get('/', (req, res) => {
-    res.render("home");
+    res.render("home", {
+        user :req.user,
+    });
 })
-
 
 app.listen(PORT, () => {
     console.log(`SERVER HAS STARTED AT THE PORT ${PORT}`);
