993c46da13e84f71567b2e04a8da287abc2b1cb1
simee56 <pandeysimee7@gmail.com>
2025-03-02 22:41:03 +0530

    changes

--- routes/blog.js
+++ routes/blog.js
@@ -1,13 +1,39 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const router = express.Router();
+
+const Blog = require('../models/blog');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve("./public/uploads"));
+    },
+    filename: function (req, file, cb) {
+        const filename = `${Date.now()}-${file.originalname}`;
+        cb(null, filename);
+    },
+});
+
+const upload = multer({ storage: storage })
 
 const { handleBlogRoute } = require('../controllers/blog');
 
 router.get('/addnewblog', handleBlogRoute);
 
 
-router.post('/', (req,res) => {
-    console.log(req.body);
-    return res.redirect('/')
-})
+router.post('/', upload.single("coverImage"), async (req, res) => {
+        const { title, bodyContent } = req.body;
+        if (!title || !bodyContent) {
+            return res.status(400).send("Title and content are required");
+        }
+        const blog = await Blog.create({
+            title,
+            bodyContent,
+            createdBy: req.user._id,
+            coverImage: `/uploads/${req.file.filename}`
+        });
+
+        return res.redirect(`/blog/${blog._id}`);
+    })
 module.exports = router;
