637b88a05bc7d172d1dd5c43df42c952c80aaab7
simee56 <pandeysimee7@gmail.com>
2025-03-08 21:49:55 +0530

    changes

--- routes/comment.js
+++ routes/comment.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const Comment = require('../models/comments');
-
 const { handleComment } = require('../controllers/comment');
 
 router.post("/comment/:blogId", handleComment);
