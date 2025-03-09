b193b9abcab0683801674372cd326a771428b4dd
Simee Pandey <164655969+simee56@users.noreply.github.com>
2025-03-09 14:38:59 +0530

    Delete routes/comment.js

--- routes/comment.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { handleComment } = require('../controllers/comment');
-
-router.post("/comment/:blogId", handleComment);
-
-module.exports = router;
