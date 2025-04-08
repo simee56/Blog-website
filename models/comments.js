a529706af41b3a4a3aa2163ae672ec2dfff6127b
simee56 <pandeysimee7@gmail.com>
2025-04-08 23:10:02 +0530

    changes

--- models/comments.js
+++ models/comments.js
@@ -14,7 +14,7 @@ const commentSchema = new Schema({
         ref: "user",
     }
 },
-    { timestamps: true }
+    { timestamps: true }         
 );
 
 const Comment = model('comment', commentSchema);
